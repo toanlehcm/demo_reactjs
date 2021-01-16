8c5fcc991d47a1739ebd0641e3e868ea1bc45275
ToanLeThanh <thanhtoanhcm1602@gmail.com>
2021-01-16 16:00:47 +0700

    props validation

--- src/App.js
+++ src/App.js
@@ -1,41 +1,53 @@
 import React from "react";
+import PropTypes from 'prop-types'; //https://stackoverflow.com/questions/46722788/reactjs-typeerror-cannot-read-property-array-of-undefined
 
 class App extends React.Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         header: "Header from props...",
-         content: "Content from props..."
-      }
-   }
-
    render() {
       return (
          <div>
-            <Header headerProp = {this.state.header}/>
-            <Content contentProp = {this.state.content}/>
+            <h3>Array: {this.props.propArray}</h3>
+            <h3>Bool: {this.props.propBool ? "True" : "False"}</h3>
+            <h3>Func: {this.props.propFunc(3)}</h3>
+            {/* <h3>Func: {this.props.propFunc()}</h3> */}
+            <h3>Number: {this.props.propNumber}</h3>
+            <h3>String: {this.props.propString}</h3>
+            <h3>Object: {this.props.propObject.objectName1}</h3>
+            <h3>Object: {this.props.propObject.objectName2}</h3>
+            <h3>Object: {this.props.propObject.objectName3}</h3>
          </div>
       );
    }
 }
 
-class Header extends React.Component {
-   render() {
-      return (
-         <div>
-            <h1>{this.props.headerProp}</h1>
-         </div>
-      );
-   }
+App.propTypes = {
+   propArray: PropTypes.array.isRequired,
+   propBool: PropTypes.bool.isRequired,
+   propFunc: PropTypes.func,
+   propNumber: PropTypes.number,
+   propString: PropTypes.string,
+   propObject: PropTypes.object
 }
 
-class Content extends React.Component {
-   render () {
-      return (
-         <div>
-            <h2>{this.props.contentProp}</h2>
-         </div>
-      );
+App.defaultProps = {
+   // propArray: [],
+   // propBool: false,
+   // propFunc: function(e) {return e},
+   // propNumber: "1",
+   // propString: 1,
+   // propObject: {
+   //    objectName1: "obj_1",
+   //    objectName2: "",
+   // }
+   propArray: [1,2,3,4,5],
+   propBool: true,
+   propFunc: function(e){return e},
+   propNumber: 1,
+   propString: "String value...",
+   
+   propObject: {
+      objectName1:"objectValue1",
+      objectName2: "objectValue2",
+      objectName3: "objectValue3"
    }
 }
 
