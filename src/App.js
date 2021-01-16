23dab058407ab811178e8e05a8b9a807ebec426f
ToanLeThanh <thanhtoanhcm1602@gmail.com>
2021-01-16 11:18:25 +0700

    props - default props

--- src/App.js
+++ src/App.js
@@ -11,4 +11,9 @@ class App extends React.Component {
    }
 }
 
+App.defaultProps = {
+   headerProp: "Header from props...",
+   contentProp: "Content from props..."
+}
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App headerProp = "Header ffom props.." contentProp = "Content from props..."/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
 
 // export default App; // export do not working so using document.getEmplementById('app')
