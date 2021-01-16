998ead8a33b6fc5e04c48904cb6a0852f43506b2
ToanLeThanh <thanhtoanhcm1602@gmail.com>
2021-01-16 16:50:40 +0700

    force update

--- src/App.js
+++ src/App.js
@@ -4,25 +4,18 @@ class App extends React.Component {
    constructor() {
       super();
 
-      this.state = {
-         data: []
-      }
-
-      this.setStateHandler = this.setStateHandler.bind(this);
+      this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
    };
 
-   setStateHandler() {
-      var item = 'setState..';
-      var myArray = this.state.data.slice();
-      myArray.push(item);
-      this.setState({data: myArray})
+   forceUpdateHandler() {
+      this.forceUpdate();
    };
 
-   render () {
+   render() {
       return (
          <div>
-            <button onClick = {this.setStateHandler}>set state</button>
-            <h4>State Array: {this.state.data}</h4>
+            <button onClick={this.forceUpdateHandler}>force update</button>
+            <h4>random number: {Math.random()}</h4>
          </div>
       );
    }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,3 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
-// export default App; // export do not working so using document.getEmplementById('app')
