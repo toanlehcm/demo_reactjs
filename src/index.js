ee1ebc58a2c614d5116759b65804e37dc1065dcc
ToanLeThanh <thanhtoanhcm1602@gmail.com>
2021-01-19 16:32:16 +0700

    simple state

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+// import App from "./App";
 import './style.css';
 
-function Item(pro) {
-    return <div className="item">
-        <b>Name:</b> {pro.name} <br />
-        <b>Price:</b> ${pro.price}
-    </div>;
+class Hello extends React.Component {
+    state = {
+        name: "Toan"
+    }
+
+    render() {
+        return <h1>Hello {this.state.name}</h1>
+    }
 }
 
-function ItemList() {
-    return <div>
-        <Item name="Cheese" price="4.99" />
-        <Item name="Bread" price="1.5" />
-        <Item name="Ice cream" price="24" />
-    </div>;
-}
-
-// const el = <ItemList />;
-
-ReactDOM.render(<ItemList />, document.getElementById('app'));
+ReactDOM.render(<Hello />, document.getElementById('app'));
