f2d4cee559d5c2053e6349d9757f4d14e9d564c6
ToanLeThanh <thanhtoanhcm1602@gmail.com>
2021-01-19 16:14:34 +0700

    item list example

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import './style.css';
 
-function Hello (props) {
-    return <h1>Hello {props.name}</h1>;
-}
-
-function NameList() {
-    return <div>
-        <Hello name="David" />
-        <Hello name="James" />
-        <Hello name="Amy" />
+function Item(props) {
+    return <div className="item">
+        <b>Name:</b> {props.name} <br />
+        <b>Price:</b> ${props.price}
     </div>;
 }
 
-ReactDOM.render(NameList(), document.getElementById('app'));
+function ItemList() {
+    return <div>
+        <Item name="Cheese" price="4.99" />
+        <Item name="Bread" price="1.5" />
+        <Item name="Ice cream" price="24" />
+    </div>;
+}
+
+const el = <ItemList />;
+
+ReactDOM.render(el, document.getElementById('app'));
