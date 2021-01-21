c5484974e98067bc91f18e0ad09726a4aca2dbff
ToanLeThanh <thanhtoanhcm1602@gmail.com>
2021-01-21 18:32:47 +0700

    add react-bootstrap-sweetalert

--- src/components/AddUserForm.js
+++ src/components/AddUserForm.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { addUser } from "../action";
+import SweetAlert  from 'react-bootstrap-sweetalert';
 
 function AddUserForm(props) {
     var [user, setUser] = useState('');
+    var [alert, setAlert] = useState(false);
 
     function editField(e) {
         setUser(e.target.value);
@@ -17,12 +19,40 @@ function AddUserForm(props) {
         e.preventDefault();
     }
 
+    function handleDelete(e){
+        const getAlert = () => (
+            <SweetAlert
+            danger
+            //warning
+            showCancel
+            confirmBtnText="OK, delete it!"
+            confirmBtnBsStyle="danger"
+            title="Are you sure you want to delete?"
+            cancelBtnBsStyle="default"
+            onConfirm={onCancelDelete}
+            onCancel={onCancelDelete}
+            focusCancelBtn
+            >
+            You will not be able to recover this product!
+        </SweetAlert>
+        );
+
+        setAlert(getAlert);
+    
+        e.preventDefault();
+    }
+
+    function onCancelDelete(){
+        setAlert(null);
+    }
+
     return (<div>
         <h1>Contact Manager</h1>
 
         <form onSubmit={handleSubmit}>
             <input type="text" value={user.name} onChange={editField} />
             <button type="submit">Add</button>
+            <button type='submit' onClick={handleDelete}>Delete</button> {alert}
         </form>
     </div>);
 }
