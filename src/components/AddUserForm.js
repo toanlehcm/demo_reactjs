22b00256b9733990f00f6d2bc56a4ee5bbbff1ce
ToanLeThanh <thanhtoanhcm1602@gmail.com>
2021-01-22 14:08:51 +0700

    add formik lib

--- src/components/AddUserForm.js
+++ src/components/AddUserForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { addUser } from "../action";
-import SweetAlert  from 'react-bootstrap-sweetalert';
+import SweetAlert from 'react-bootstrap-sweetalert';
+import { Formik } from "formik";
 
 function AddUserForm(props) {
     var [user, setUser] = useState('');
@@ -19,41 +20,82 @@ function AddUserForm(props) {
         e.preventDefault();
     }
 
-    function handleDelete(e){
+    function handleDelete(e) {
         const getAlert = () => (
             <SweetAlert
-            danger
-            //warning
-            showCancel
-            confirmBtnText="OK, delete it!"
-            confirmBtnBsStyle="danger"
-            title="Are you sure you want to delete?"
-            cancelBtnBsStyle="default"
-            onConfirm={onCancelDelete}
-            onCancel={onCancelDelete}
-            focusCancelBtn
+                danger
+                //warning
+                showCancel
+                confirmBtnText="OK, delete it!"
+                confirmBtnBsStyle="danger"
+                title="Are you sure you want to delete?"
+                cancelBtnBsStyle="default"
+                onConfirm={onCancelDelete}
+                onCancel={onCancelDelete}
+                focusCancelBtn
             >
-            You will not be able to recover this product!
-        </SweetAlert>
+                You will not be able to recover this product!
+            </SweetAlert>
         );
 
         setAlert(getAlert);
-    
+
         e.preventDefault();
     }
 
-    function onCancelDelete(){
+    function onCancelDelete() {
         setAlert(null);
     }
 
     return (<div>
         <h1>Contact Manager</h1>
+        <Formik
+            initialValues={{ email: '', password: '' }}
 
-        <form onSubmit={handleSubmit}>
-            <input type="text" value={user.name} onChange={editField} />
-            <button type="submit">Add</button>
-            <button type='submit' onClick={handleDelete}>Delete</button> {alert}
-        </form>
+            validate={values => {
+                const errors = {};
+                if (!values.email) {
+                    errors.email = 'Required';
+                } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                    errors.email = 'Invalid email address';
+                }
+
+                return errors;
+            }}
+
+            onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    setSubmitting(false);
+                }, 400);
+            }}
+        >
+
+            {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+            }) => (
+                <form onSubmit={handleSubmit}>
+                    <p>
+                        <input type="email" name="email" value={user.name} onChange={editField} onBlur={handleBlur} />
+                        <p>{errors.email && touched.email && errors.email}</p>
+                    </p>
+
+                    <p>
+                        <input type="password" name="password" value={values.password} onChange={handleChange} onBlur={handleBlur} />
+                        <p>{errors.password && touched.password && errors.password}</p>
+                    </p>
+
+                    <button type="submit" disabled={isSubmitting}>Submit</button>
+                    <button type='submit' onClick={handleDelete}>Delete</button> {alert}
+                </form>
+            )}
+        </Formik>
     </div>);
 }
 
