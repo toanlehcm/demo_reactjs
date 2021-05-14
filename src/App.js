96e7cae9f3689e1a5dbfe9cc6ee34660801a7110
Toan Le <thanhtoanhcm1602@gmai.com>
2021-05-14 08:33:31 +0700

    - add reactstrap.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
 import React, { useState } from "react";
-import './style.css';
+import { Navbar, NavbarBrand } from "reactstrap";
+import "./style.css";
 
 class App extends React.Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            data: []
-        }
+    this.state = {
+      data: [],
+    };
 
-        this.listUp = this.listUp.bind(this);
-    }
+    this.listUp = this.listUp.bind(this);
+  }
 
-    listUp() {
-        var arr = ["a", "b", "c"];
-        var myArr = arr.map((val, index) => <li key={index}>{val}</li>);
-        this.setState({ data: myArr });
-    }
+  listUp() {
+    var arr = ["a", "b", "c"];
+    var myArr = arr.map((val, index) => <li key={index}>{val}</li>);
+    this.setState({ data: myArr });
+  }
 
-    render() {
-        return (
-            <div>
-                <button onClick={this.listUp} >list up</button>
-                <p>list: {this.state.data} </p>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">React</NavbarBrand>
+          </div>
+          <div>
+            <button onClick={this.listUp}>list up</button>
+            <p>list: {this.state.data} </p>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
 import AddUserForm from "./components/AddUserForm";
 import GetUserList from "./components/GetUserList";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import "./style.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'jquery/dist/jquery.min.js';
+// import 'jquery/dist/jquery.min.js';
 
 const initialState = {
-    contacts: ["James Smith", "Thomas Anderson", "Bruce Wayne"]
+  contacts: ["James Smith", "Thomas Anderson", "Bruce Wayne"],
 };
 
 // Reducer function
 function reducer(state = initialState, action) {
-    switch (action.type) {
-        case 'ADD_USER':
-            return { ...state, contacts: [...state.contacts, action.data] }
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "ADD_USER":
+      return { ...state, contacts: [...state.contacts, action.data] };
+    default:
+      return state;
+  }
 }
 
 // create store
@@ -27,8 +28,10 @@ const store = createStore(reducer);
 
 // pass the store to our components
 ReactDOM.render(
-    <Provider store={store}>
-        <AddUserForm />
-        <GetUserList />
-    </Provider>,
-    document.getElementById('app'));
+  <Provider store={store}>
+    <App />
+    <AddUserForm />
+    <GetUserList />
+  </Provider>,
+  document.getElementById("app")
+);
