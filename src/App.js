b439bf81cb67c5c3555442951fb0606b0b615e42
Toan Le <thanhtoanhcm1602@gmai.com>
2021-05-17 12:37:41 +0700

    + assignment 1.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/MenuComponent";
 import { DISHES } from "./shared/dishes";
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,41 +1,22 @@
 import React, { Component } from "react";
-import {
-  Card,
-  CardImg,
-  CardImgOverlay,
-  CardText,
-  CardBody,
-  CardTitle,
-} from "reactstrap";
+import DishDetail from "./DishdetailComponent";
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 class Menu extends Component {
   constructor(props) {
     super(props);
     this.state = {
       selectedDish: null,
     };
+    console.log("Menu component constructed");
   }
 
   onDishSelect(dish) {
     this.setState({ selectedDish: dish });
   }
 
-  renderDish(dish) {
-    if (dish != null) {
-      return (
-        <Card>
-          <CardImg top src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    } else {
-      return <div></div>;
-    }
-  }
-
   render() {
+    console.log("renders menu component");
+
     const menu = this.props.dishes.map((dish) => {
       return (
         <div className="col-12 col-md-5 m-1">
@@ -52,13 +33,13 @@ class Menu extends Component {
     return (
       <div className="container">
         <div className="row">{menu}</div>
-        <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            {this.renderDish(this.state.selectedDish)}
-          </div>
-        </div>
+        <DishDetail dish={this.state.selectedDish} />
       </div>
     );
+  }
+
+  componentDidMount() {
+    console.log("Menu component componentDidMount is invoked");
   }
 }
 
