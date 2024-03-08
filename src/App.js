ae1e1219fbf20db11ad17359e05bbcc0257d3d79
Toan Le <thanhtoanhcm1602@gmail.com>
2024-03-08 14:35:08 +0700

    - Demo history library but not work.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import HomePage from './containers/Home/HomePage';
-import AboutPage from './containers/About/AboutPage';
-import ContactPage from './containers/Contact/Contact';
+import React from 'react';
+import { createBrowserHistory } from 'history';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+
+const history = createBrowserHistory();
 
 // Define component.
-const Home = () => <h1>Home</h1>
-const About = () => <h1>About</h1>
-const Contact = () => <h1>Contact</h1>
+const Home = () => {
+  // console.log('Rendering Home');
+  return <h1>Home</h1>;
+}
+
+const About = () => {
+  // console.log('Rendering About');
+  return <h1>About</h1>;
+}
+
+const Contact = () => {
+  // console.log('Rendering Contact');
+  return <h1>Contact</h1>;
+}
 
 function App() {
+  // console.log('Current pathname:', history.location.pathname);
+
+  /*------- React router. ---------*/
   return (
     <Router>
       <div>
@@ -21,16 +36,43 @@ function App() {
             <li><Link to='/contact'>Contact</Link></li>
           </ul>
         </nav>
-
-        {/* Define routes. Each route component defines a mapping between a path and a component. */}
         <Routes>
-          <Route path='/' element={<HomePage><Home /></HomePage>} />
-          <Route path='/about' element={<AboutPage><About /></AboutPage>} />
-          <Route path='/contact' element={<ContactPage><Contact /></ContactPage>} />
+          <Route path="/" exact element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
     </Router>
   );
+
+  /*------- History library. ---------*/
+  // return (
+  //   <Router>
+  //     <div>
+  //       <nav>
+  //         <ul>
+  //           {/* Define navigation links. */}
+  //           <li><Link to="/">Home</Link></li>
+  //           <li><Link to="/about">About</Link></li>
+  //         </ul>
+  //       </nav>
+
+  //       {/* Render components based on the current pathname */}
+  //       <Routes />
+  //     </div>
+  //   </Router>
+  // ); 
 }
+
+// function Routes() {
+//   const location = useLocation();
+
+//   return(
+//     <>
+//       {history.location.pathname === '/' && <Home />}
+//       {history.location.pathname === '/about' && <About />}
+//     </>
+//   );
+// }
 
 export default App;
