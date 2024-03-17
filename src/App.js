951f2ff5e578d14c74252be6f1355c94f56ab4e0
Toan Le <thanhtoanhcm1602@gmail.com>
2024-03-17 15:51:21 +0700

    - Demo Object's assign method.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React from 'react';
 
 export default function App() {
-  // Define a function constructor.
-  function Person(name, age) {
-    this.name = name;
-    this.age = age;
-  }
 
-  // Adding a method to the prototype.
-  Person.prototype.sayHello = function () {
-    console.log(`Name is ${this.name} and ${this.age} years old`);
-  };
+  const orgObj = { company: 'qwe crop' };
+  const carObj = { name: 'Honda' };
 
-  // Create a new instance using Object.create and constructor's prototype.
-  const newInstance = Object.create(Person.prototype);
-
-  // Call the function with the instance and parameters.
-  var result = Person.call(newInstance, 'John', 30);
-
-  // If the result is a non-null object then use it otherwise just use the new instance.
-  var finalInstance = result && typeof result === 'object' ? result : newInstance;
-
-  console.log('newInstance', newInstance, newInstance.name, newInstance.age, newInstance.sayHello());
-  console.log('result', result);
-  console.log('final', finalInstance, finalInstance.name, finalInstance.age, finalInstance.sayHello());
+  // const staff = Object.assign(orgObj, carObj);
+  const staff = Object.assign({}, orgObj, carObj);
+  console.log('orgObj', orgObj);
+  console.log('carObj', carObj);
+  console.log('staff', staff);
 
   return (<></>);
 }
