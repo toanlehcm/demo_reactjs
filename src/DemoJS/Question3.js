f7c7e7cfade6e809d751222eaf88d756e0c77ddc
Toan Le <thanhtoanhcm1602@gmail.com>
2024-03-17 17:45:30 +0700

    - Demo 3. What is the difference between Call, Apply and Bind: + Bind: returns a new function, allowing you to pass any number of arguments.

--- src/DemoJS/Question3.js
+++ src/DemoJS/Question3.js
@@ -1,11 +1,16 @@
-// 3. What is the difference between Call, Apply and Bind.
+/** 3. What is the difference between Call, Apply and Bind.
+ * https://www.w3schools.com/js/js_function_call.asp
+ * https://www.w3schools.com/js/js_function_apply.asp
+ * https://www.w3schools.com/js/js_function_bind.asp
+ * */
+
 export default function Question3() {
 
   var employee1 = { firstName: 'john', lastName: 'Rodson' };
   var employee2 = { firstName: 'Jimmy', lastName: 'baily' };
 
   function invite(greeting1, greeting2) {
-    console.log(greeting1 + " " + this.firstName + " " + this.lastName + ", " + greeting2);
+    console.log(this.firstName + " " + this.lastName + ", " + greeting1 + " " + greeting2);
   }
 
   // Call(). 
@@ -15,4 +20,11 @@ export default function Question3() {
   // Apply().
   invite.apply(employee1, ["try", "learning"]);
   invite.apply(employee2, ["practice", "studying"]);
+
+  // Bind().
+  var inviteEmployee1 = invite.bind(employee1);
+  var inviteEmployee2 = invite.bind(employee2);
+
+  inviteEmployee1('never', 'give up');
+  inviteEmployee2('try', 'hard');
 }
