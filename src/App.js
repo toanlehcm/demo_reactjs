2eb9544d9c3f134e8e656a7c8ce7c87313f9863f
Toan Le <thanhtoanhcm1602@gmail.com>
2024-03-17 15:56:42 +0700

    - Demo ES6 Class syntax.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 export default function App() {
 
-  const orgObj = { company: 'qwe crop' };
-  const carObj = { name: 'Honda' };
+  class Person {
+    constructor(name) {
+      this.name = name;
+    }
+  }
 
-  // const staff = Object.assign(orgObj, carObj);
-  const staff = Object.assign({}, orgObj, carObj);
-  console.log('orgObj', orgObj);
-  console.log('carObj', carObj);
-  console.log('staff', staff);
+  var object = new Person('John');
+  console.log('object', object);
 
   return (<></>);
 }
