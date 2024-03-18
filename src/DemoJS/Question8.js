02523ce7c267d0741b980da4355a835ef84009f8
Toan Le <thanhtoanhcm1602@gmail.com>
2024-03-18 12:36:18 +0700

    - 8. Object keys may collide with default keys in prototype.

--- src/DemoJS/Question8.js
+++ src/DemoJS/Question8.js
@@ -21,18 +21,42 @@ export default function Question8() {
 
   /*------ Keys in a Map are ordered while keys added to an Object are not: ------*/
   // Object keys order is not guaranteed(đảm bảo).
-  let obj = {};
-  console.log('obj', obj);
-  obj['z'] = 1;
-  obj['a'] = 2;
-  console.log('ob*j', obj.size);
+  // let obj = {};
+  // // console.log('obj', obj);
+  // obj['z'] = 1;
+  // obj['a'] = 2;
+  // // console.log('ob*j', obj.size);
 
-  //  Map keys are ordered.
-  let map = new Map();
-  console.log('map', map);
-  map.set('z', 1);
-  map.set('a', 2);
+  // //  Map keys are ordered.
+  // let map = new Map();
+  // // console.log('map', map);
+  // map.set('z', 1);
+  // map.set('a', 2);
 
-  // Getting the size of a Map.
-  console.log('map*', map.size);
+  // // Getting the size of a Map.
+  // // console.log('map*', map.size);
+
+  // // Iterating over a Map.
+  // for (let [key, value] of map) {
+  //   console.log(key, value);
+  // }
+
+  /*------ Object keys may collide with default keys in prototype: ------*/
+  // Creating an object without a prototype
+  let obj = Object.create(null);
+  console.log('obj-1', obj);
+
+  // Adding key-value pairs to the object
+  obj['toString'] = 'overwritten';
+  obj['valueOf'] = 'value';
+  console.log('obj-2', obj);
+
+  // Accessing key-value pairs
+  console.log(obj['toString'], obj['valueOf']); // Output: overwritten, value
+
+  // Object key.
+  console.log(Object.keys(obj));
+
+  // Object prototype.
+  console.log(Object.getPrototypeOf(obj));
 }
