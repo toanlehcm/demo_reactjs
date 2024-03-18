44fa000a1e6b428ea94f9236b3618844e2ba7576
Toan Le <thanhtoanhcm1602@gmail.com>
2024-03-18 11:00:45 +0700

    - 8. Keys in a Map are ordered while keys added to an Object are not.

--- src/DemoJS/Question8.js
+++ src/DemoJS/Question8.js
@@ -1,21 +1,36 @@
 export default function Question8() {
   /*------ Keys of an Object can be Strings and Symbols, whereas keys of a Map can be any value: ------*/
   // Object with string key.
-  let objectKey = {};
-  objectKey['foo'] = 'bar';
-  console.log('objectKey', objectKey);
+  // let objectKey = {};
+  // objectKey['foo'] = 'bar';
+  // // console.log('objectKey', objectKey);
 
-  // Object with symbol key.
-  let symbolKey = Symbol();
+  // // Object with symbol key.
+  // let symbolKey = Symbol();
+  // let obj = {};
+  // obj[symbolKey] = 'value';
+  // // console.log('obj', obj);
+
+  // // Map with key types.
+  // let map = new Map();
+  // // console.log('map', map);
+  // map.set('stringKey', 'value');
+  // map.set(symbolKey, 'value');
+  // map.set(obj, 'value');
+  // console.log('map*', map);
+
+  /*------ Keys in a Map are ordered while keys added to an Object are not: ------*/
+  // Object keys order is not guaranteed(đảm bảo).
   let obj = {};
-  obj[symbolKey] = 'value';
   console.log('obj', obj);
+  obj['z'] = 1;
+  obj['a'] = 2;
+  console.log('ob*j', obj);
 
-  // Map with key types.
+  //  Map keys are ordered.
   let map = new Map();
   console.log('map', map);
-  map.set('stringKey', 'value');
-  map.set(symbolKey, 'value');
-  map.set(obj, 'value');
+  map.set('z', 1);
+  map.set('a', 2);
   console.log('map*', map);
 }
