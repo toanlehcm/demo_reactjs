f8aaf68232703219926b749cacc6cbf303dad5a9
Toan Le <thanhtoanhcm1602@gmail.com>
2024-03-24 15:05:23 +0700

    - 4. Fibonacci Sequence.

--- src/LeetCode/CommonlyExercises.js
+++ src/LeetCode/CommonlyExercises.js
@@ -27,11 +27,39 @@ export default function CommonlyExercises() {
   function factorial(n) {
     if (n === 0) return 1;
 
-    console.log('n', n);
+    // console.log('n', n);
 
     return n * factorial(n - 1);
   }
 
   var result = factorial(5);
-  console.log('result', result);
+  // console.log('result', result);
+
+  /**
+   * 4. Fibonacci Sequence:
+    Generate the Fibonacci sequence up to a certain number of terms.
+    https://iviettech.vn/blog/hoc_thuat_toan/10941-thuat-toan-ve-day-so-fibonacci.html
+    Dãy fibonacci bắt đầu với hai phần tử f(1)=1, f(2)=1
+    Số tiếp theo sẽ bằng tổng của hai số trước đó f(n) = f(n-1) + f(n-2)
+   * */
+  function fibonacci(n) {
+    // Initialize an array to store the Fibonacci sequence, starting with the first two numbers.
+    let sequence = [0, 1];
+
+    // Iterate starting from the third number up to the nth number in the Fibonacci sequence.
+    for (let i = 2; i < n; i++) {
+      // Calculate the next Fibonacci number by adding the last two numbers in the sequence.
+      console.log('i', i, 'sequence', sequence, sequence[i - 1], sequence[i - 2]);
+      let nextFibonacciNumber = sequence[i - 1] + sequence[i - 2];
+
+      // Add the newly calculated Fibonacci number to the sequence array.
+      sequence.push(nextFibonacciNumber);
+    }
+
+    // Return the generated Fibonacci sequence array.
+    console.log(sequence);
+    // return sequence;
+  }
+
+  fibonacci(8);
 }
