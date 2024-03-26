02d8707a4df96ae714a38cd1a00673f0dcbd03fd
Toan Le <thanhtoanhcm1602@gmail.com>
2024-03-27 06:10:15 +0700

    - Function scope.

--- src/DemoJS/ArrowAndRegularFunctions.js
+++ src/DemoJS/ArrowAndRegularFunctions.js
@@ -20,26 +20,70 @@ export default function ArrowAndRegularFunctions(params) {
   // console.log(objArrow.greetArrow());
 
   /*---------------- 'this' in regular function. --------------------*/
-  function Person(name) {
-    this.name = name;
+  // function Person(name) {
+  //   this.name = name;
+  // }
+
+  // const john = new Person('John');
+  // console.log('john.name', john.name); // Output: John
+
+  // const greet = function () {
+  //   console.log('Hello, ' + this.name);
+  // };
+
+  // const obj = {
+  //   name: 'Jane'
+  // };
+
+  // // Call 'greet' as a method of 'obj'
+  // obj.greet = greet;
+  // console.log('obj.greet()', obj.greet()); // Output: Hello, Jane
+
+  // // Call 'greet' as a standalone function
+  // console.log('greet()', greet());
+  // Output: Hello, undefined (or the value of 'name' from the global scope)
+
+  /*---------------- Function scope. --------------------*/
+  const obj = {
+    name: 'John',
+    regularFunc: function () {
+      console.log('Regular function:', this, this.name); // 'this' refers to 'obj'
+    },
+    arrowFunc: () => {
+      console.log('Arrow function:', this ? this.name : this); // this.name: 'this' inherits from the enclosing lexical scope (likely the global scope)
+    }
+  };
+
+  function anotherFunction() {
+    const innerObj = {
+      name: 'Inner John',
+      arrowFunc: obj.arrowFunc
+    };
+
+    innerObj.arrowFunc(); // Output: Arrow function: undefined
   }
 
-  const john = new Person('John');
-  console.log('john.name', john.name); // Output: John
-
-  const greet = function () {
-    console.log('Hello, ' + this.name);
+  const anotherObj = {
+    name: 'Alice',
+    arrowFunc: obj.arrowFunc // undefined
   };
 
-  const obj = {
-    name: 'Jane'
+  const outerObj = {
+    name: 'Outer John',
+    innerObj: {
+      name: 'Inner John',
+      arrowFunc: () => {
+        console.log('Arrow function of outerObj:', this ? this.name : this); // 'this' inherits from the outer lexical scope // undefined
+      }
+    }
   };
 
-  // Call 'greet' as a method of 'obj'
-  obj.greet = greet;
-  console.log('obj.greet()', obj.greet()); // Output: Hello, Jane
+  obj.regularFunc(); // Output: Regular function: John
+  obj.arrowFunc(); // Output: Arrow function: undefined (or the value of 'name' from the global scope)
 
-  // Call 'greet' as a standalone function
-  console.log('greet()', greet());
-  // Output: Hello, undefined (or the value of 'name' from the global scope)
+  // If 'obj.arrowFunc()' is called within another function where 'this' is defined, it will inherit 'this' from that scope.
+  anotherFunction(); // Output: Arrow function: undefined.
+  console.log('anotherObj.arrowFunc()', anotherObj.arrowFunc()); // Output: Arrow function: undefine
+
+  console.log('outerObj.innerObj.arrowFunc()', outerObj.innerObj.arrowFunc()); // Output: Arrow function: undefine
 }
