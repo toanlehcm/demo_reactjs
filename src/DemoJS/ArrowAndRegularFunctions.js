af3876a69255a3945eb27afc3900cb8bb66b6852
Toan Le <thanhtoanhcm1602@gmail.com>
2024-03-27 04:59:37 +0700

    - 'this' in regular function.

--- src/DemoJS/ArrowAndRegularFunctions.js
+++ src/DemoJS/ArrowAndRegularFunctions.js
@@ -1,20 +1,45 @@
 export default function ArrowAndRegularFunctions(params) {
-  const obj = {
-    name: 'John',
-    greet: function () {
-      // Regular function
-      console.log('Hello, ' + this.name);
-    },
-    greetArrow: () => {
-      // Arrow function
-      // 'this' refers to the global object, not 'obj'
-      console.log('Hello, ' + this.name);
-    }
-  };
+
+  /*---------------- 'this' in arrow function. --------------------*/
+  // const objArrow = {
+  //   name: 'John',
+  //   greet: function () {
+  //     // Regular function
+  //     console.log('Hello, ' + this.name);
+  //   },
+  //   greetArrow: () => {
+  //     // Arrow function
+  //     // 'this' refers to the global object, not 'objArrow'
+  //     console.log('Hello, ' + this.name);
+  //   }
+  // };
 
   // Output: Hello, John
-  console.log(obj.greet());
+  // console.log(objArrow.greet());
   // Output: Hello, undefined (or the value of 'name' from the global scope)
-  console.log(obj.greetArrow());
+  // console.log(objArrow.greetArrow());
 
+  /*---------------- 'this' in regular function. --------------------*/
+  function Person(name) {
+    this.name = name;
+  }
+
+  const john = new Person('John');
+  console.log('john.name', john.name); // Output: John
+
+  const greet = function () {
+    console.log('Hello, ' + this.name);
+  };
+
+  const obj = {
+    name: 'Jane'
+  };
+
+  // Call 'greet' as a method of 'obj'
+  obj.greet = greet;
+  console.log('obj.greet()', obj.greet()); // Output: Hello, Jane
+
+  // Call 'greet' as a standalone function
+  console.log('greet()', greet());
+  // Output: Hello, undefined (or the value of 'name' from the global scope)
 }
