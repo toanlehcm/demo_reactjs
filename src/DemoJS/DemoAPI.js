cf3f6b42182d5c9f57ba20f36c2242f661215906
Toan Le <thanhtoanhcm1602@gmail.com>
2024-03-27 09:46:42 +0700

    - Demo Use the axios.

--- src/DemoJS/DemoAPI.js
+++ src/DemoJS/DemoAPI.js
@@ -1,6 +1,8 @@
 // https://www.w3schools.com/jsref/api_fetch.asp
+import axios from "axios";
 
 export default function DemoAPI(params) {
+
   /*--- Use the fetch() function. ---*/
   fetch('https://js-post-api.herokuapp.com/api/posts?_page=1&limit=10', {
     method: "GET",
@@ -18,15 +20,25 @@ export default function DemoAPI(params) {
         throw new Error('Network response was not ok');
       }
       // Parse the JSON response
-      console.log('response', response);
+      // console.log('response', response);
       return response.json();
     })
     .then(data => {
       // Handle the JSON data
-      console.log('data', data);
+      // console.log('data', data);
     })
     .catch(error => {
       // Handle any errors that occurred during the fetch
       console.error('Fetch error:', error);
+    });
+
+  /*--- Use the axios. ---*/
+  axios.get('https://js-post-api.herokuapp.com/api/posts?_page=1&limit=10')
+    .then(response => {
+      // Handle the response data.
+      console.log('axios response data: ', response, response.data);
+    }).catch(error => {
+      // Handle any errors that occurred during the request.
+      console.error('Axios error: ', error);
     });
 }
