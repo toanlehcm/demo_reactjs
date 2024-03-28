38c698cab1efdc17efcdaf61590926b7b87cceb6
Toan Le <thanhtoanhcm1602@gmail.com>
2024-03-29 06:22:07 +0700

    - Returns array containing duplicate numbers.

--- src/DemoJS/QuestionLogOutDuplicateNumbers.js
+++ src/DemoJS/QuestionLogOutDuplicateNumbers.js
@@ -2,7 +2,7 @@ export default function QuestionLogOutDuplicateNumbers() {
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2];
   const frequencyMap = {};
 
-  // Đếm số lần xuất hiện của mỗi phần tử trong mảng và lưu vào hashmap
+  /*--- Đếm số lần xuất hiện của mỗi phần tử trong mảng và lưu vào hashmap ---*/
   arr.forEach(num => {
     if (frequencyMap[num]) {
       frequencyMap[num]++;
@@ -10,12 +10,35 @@ export default function QuestionLogOutDuplicateNumbers() {
       frequencyMap[num] = 1;
     }
   });
-  console.log('frequencyMap', frequencyMap);
+  // console.log('frequencyMap', frequencyMap);
 
   // Log ra các số trùng lặp
   for (const num in frequencyMap) {
     if (frequencyMap[num] > 1) {
-      console.log(`${num} xuất hiện ${frequencyMap[num]} lần`);
+      // console.log(`${num} xuất hiện ${frequencyMap[num]} lần`);
     }
   }
+
+  /*--- Returns array containing duplicate numbers. ---*/
+  const arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2];
+  const frequencyMap2 = {};
+  const duplicates = [];
+
+  // Đếm số lần xuất hiện của mỗi phần tử trong mảng và lưu vào hashmap
+  arr2.forEach(num => {
+    if (frequencyMap2[num]) {
+      frequencyMap2[num]++;
+    } else {
+      frequencyMap2[num] = 1;
+    }
+  });
+
+  // Lọc ra các phần tử trùng lặp và thêm vào mảng duplicates
+  for (const num in frequencyMap2) {
+    if (frequencyMap2[num] > 1) {
+      duplicates.push(parseInt(num)); // Chuyển đổi từ string sang number
+    }
+  }
+
+  console.log(duplicates); // Output: [1, 2]
 }
