6f754b23db78804c216e279604a0d09b0691b7d6
Toan Le <thanhtoanhcm1602@gmail.com>
2024-03-29 06:23:08 +0700

    - Adjust code.

--- src/DemoJS/QuestionLogOutDuplicateNumbers.js
+++ src/DemoJS/QuestionLogOutDuplicateNumbers.js
@@ -1,8 +1,8 @@
 export default function QuestionLogOutDuplicateNumbers() {
+  /*--- Đếm số lần xuất hiện của mỗi phần tử trong mảng và lưu vào hashmap ---*/
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2];
   const frequencyMap = {};
 
-  /*--- Đếm số lần xuất hiện của mỗi phần tử trong mảng và lưu vào hashmap ---*/
   arr.forEach(num => {
     if (frequencyMap[num]) {
       frequencyMap[num]++;
