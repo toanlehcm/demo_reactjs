b413b7900494da8cb189352b106d921d7c873271
Toan Le <thanhtoanhcm1602@gmail.com>
2024-03-29 04:57:24 +0700

    - Apply closure in localStorage.

--- src/DemoJS/DemoClosure.js
+++ src/DemoJS/DemoClosure.js
@@ -25,10 +25,48 @@ export default function DemoClosure(params) {
   /*----------- Demo 2. -----------*/
   function debug(name) {
     return function (str) {
-      console.log(`${name} ${str}`);
+      // console.log(`${name} ${str}`);
     }
   }
 
   var log = debug('Hello'); // Vì debug return ve 1 hàm, nen log lúc này là 1 hàm.
-  log('world'); // log nhận 'world' như parmas str.
+  // log('world'); // log nhận 'world' như parmas str.
+
+  /*----------- Apply localStorage. -----------*/
+  /**
+   * localStorage lưu: 
+   *    option chọn theme sáng tối.
+   *    option chọn cách hiển thị dạng list hay grid.
+   *    các yếu tố có thể lưu ở client.
+   * */
+  function createStorage(key) {
+    const store = JSON.parse(localStorage.getItem(key)) ?? {};
+
+    const save = () => {
+      localStorage.setItem(key, JSON.stringify(store));
+    }
+
+    // Cả 3 hàm đều tham chiếu đến store bên ngoài phạm vi storage.
+    const storage = {
+      get(key) {
+        return store[key];
+      },
+      set(key, value) {
+        store[key] = value;
+        save();
+      },
+      remove(key) {
+        delete store[key];
+        save();
+      }
+    }
+
+    return storage;
+  }
+
+  // Profile.js
+  const profileSetting = createStorage('profile_setting');
+  profileSetting.set('fullName', 'Toan');
+  profileSetting.set('age', 31);
+  console.log(profileSetting.get('fullName'));
 }
