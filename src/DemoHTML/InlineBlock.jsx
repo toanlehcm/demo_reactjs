a980506b1d4b256a11e09d71b35e3402686176a7
Toan Le <thanhtoanhcm1602@gmail.com>
2024-04-01 04:14:29 +0700

    - Inline-block:  Inconsistent size.

--- src/DemoHTML/InlineBlock.jsx
+++ src/DemoHTML/InlineBlock.jsx
@@ -4,19 +4,25 @@ export default function InlineBlock() {
   return (
     <>
       {/*------ 1. Not enough space in the container. ------*/}
-      <div class="container-not-enough-space">
-        <div class="inline-block-element">Element 1</div>
-        <div class="inline-block-element">Element 2</div>
-        <div class="inline-block-element">Element 3</div>
-        <div class="inline-block-element">Element 4</div>
+      <div className="container-not-enough-space">
+        <div className="inline-block-element">Element 1</div>
+        <div className="inline-block-element">Element 2</div>
+        <div className="inline-block-element">Element 3</div>
+        <div className="inline-block-element">Element 4</div>
       </div>
 
       {/*------ 2. White space in HTML code. ------*/}
-      <div class="container-white-space">
-        <div class="inline-block-element">Element 1</div>
-        <div class="inline-block-element">Element 2</div>
-        <div class="inline-block-element">Element 3</div>
-        <div class="inline-block-element">Element 4</div>
+      <div className="container-white-space">
+        <div className="inline-block-element">Element 1</div>
+        <div className="inline-block-element">Element 2</div>
+        <div className="inline-block-element">Element 3</div>
+        <div className="inline-block-element">Element 4</div>
+      </div>
+
+      {/*------ 3. Inconsistent size ------*/}
+      <div class="container-inconsistent-size">
+        <div class="inline-block-element large">Large Element</div>
+        <div class="inline-block-element small">Small Element</div>
       </div>
     </>
   );
