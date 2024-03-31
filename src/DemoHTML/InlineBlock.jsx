ac200aca57e3eb7c9f8b8b004700a808c7eb22af
Toan Le <thanhtoanhcm1602@gmail.com>
2024-04-01 03:55:35 +0700

    - 2. White space in HTML code.

--- src/DemoHTML/InlineBlock.jsx
+++ src/DemoHTML/InlineBlock.jsx
@@ -4,7 +4,15 @@ export default function InlineBlock() {
   return (
     <>
       {/*------ 1. Not enough space in the container. ------*/}
-      <div class="container">
+      <div class="container-not-enough-space">
+        <div class="inline-block-element">Element 1</div>
+        <div class="inline-block-element">Element 2</div>
+        <div class="inline-block-element">Element 3</div>
+        <div class="inline-block-element">Element 4</div>
+      </div>
+
+      {/*------ 2. White space in HTML code. ------*/}
+      <div class="container-white-space">
         <div class="inline-block-element">Element 1</div>
         <div class="inline-block-element">Element 2</div>
         <div class="inline-block-element">Element 3</div>
