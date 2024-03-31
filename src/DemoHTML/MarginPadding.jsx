9c91b1b3bfb6d7d9cd71e24c9aa304d340e52696
Toan Le <thanhtoanhcm1602@gmail.com>
2024-03-31 16:12:28 +0700

    - Demo margin padding left right.

--- src/DemoHTML/MarginPadding.jsx
+++ src/DemoHTML/MarginPadding.jsx
@@ -2,9 +2,9 @@ import styles from "./MarginPadding.css";
 
 export default function MarginPadding() {
   return (
-    <>
+    <div className="div-parent">
       <div className="div1">div1</div>
       <div className="div2">div2</div>
-    </>
+    </div>
   );
 }
