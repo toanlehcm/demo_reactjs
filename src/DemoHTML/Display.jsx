c9fb27f98fa9ff2cf1226a0f20519f2c5160a3b8
Toan Le <thanhtoanhcm1602@gmail.com>
2024-04-01 15:40:36 +0700

    - Test margin, padding of inline, block, inline-block.

--- src/DemoHTML/Display.jsx
+++ src/DemoHTML/Display.jsx
@@ -1,10 +1,27 @@
-import styles from './Display.css';
+import styles from "./Display.css";
 
 export default function Display(params) {
   return (
     <>
-      <div class="container">
+      <div className="container">
         <img src="/logo192.png" alt="Your Image" />
+      </div>
+
+      <h1>The display Property</h1>
+
+      <h2>display: inline</h2>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consequat scelerisque elit sit amet consequat. Aliquam erat volutpat. <span className="a">Aliquam</span> <span className="a">venenatis</span> gravida nisl sit amet facilisis. Nullam cursus fermentum velit sed laoreet.{" "}
+      </div>
+
+      <h2>display: inline-block</h2>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consequat scelerisque elit sit amet consequat. Aliquam erat volutpat. <span className="b">Aliquam</span> <span className="b">venenatis</span> gravida nisl sit amet facilisis. Nullam cursus fermentum velit sed laoreet.{" "}
+      </div>
+
+      <h2>display: block</h2>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consequat scelerisque elit sit amet consequat. Aliquam erat volutpat. <span className="c">Aliquam</span> <span className="c">venenatis</span> gravida nisl sit amet facilisis. Nullam cursus fermentum velit sed laoreet.{" "}
       </div>
     </>
   );
