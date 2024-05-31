77a7b042564db60263f23f105a1816e58b0777c2
Toan Le <thanhtoanhcm1602@gmail.com>
2024-05-31 10:12:43 +0700

    ReactJS: 09-02 Tạo routing cho từng feature.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
-// import AlbumFeature from './features/Album';
+import AlbumFeature from './features/Album';
 import TodoFeature from './features/Todo';
+import { Routes, Route } from 'react-router-dom';
 
 export default function App() {
 
-  return (<TodoFeature />);
+  return (
+    <div className='App'>
+      Header
+      <Routes>
+        <Route path="/todos" element={<TodoFeature />} />
+        <Route path='/albums' element={<AlbumFeature />} />
+      </Routes>
+      Footer
+    </div>
+  );
 }
