39357eb39c3ca5f43ff92066b21c08f15b830c57
Toan Le <thanhtoanhcm1602@gmail.com>
2024-06-03 21:40:17 +0700

    ReactJS: 13-05 Setup counterSlice

---
+++ src/features/Counter/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CounterFeature = (props) => {
+  return <div></div>;
+};
+
+CounterFeature.propTypes = {};
+
+export default CounterFeature;
