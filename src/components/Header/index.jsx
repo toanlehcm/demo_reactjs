5d6fd621bfcf3801eb0bea5404c445611868c28c
Toan Le <thanhtoanhcm1602@gmail.com>
2024-06-04 11:38:06 +0700

    ReactJS: 15-04 Tạo dialog để hiển thị form đăng ký

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -6,8 +6,23 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import CodeIcon from '@mui/icons-material/Code';
 import { NavLink, Link } from 'react-router-dom';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason && reason === 'backdropClick') return;
+    setOpen(false);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position='static'>
@@ -29,23 +44,50 @@ export default function Header() {
             <Link to='/'>EZ SHOP</Link>
           </Typography>
 
-          {/*  activeClassName='active-menu' */}
           <NavLink to='/todos'>
             <Button color='inherit' sx={{ color: '#fff' }}>
               Todo
             </Button>
           </NavLink>
 
-          {/* activeClassName='active' */}
           <NavLink to='/albums'>
             <Button color='inherit' sx={{ color: '#fff' }}>
               Album
             </Button>
           </NavLink>
 
-          <Button color='inherit'>Register</Button>
+          <Button color='inherit' onClick={handleClickOpen}>
+            Register
+          </Button>
         </Toolbar>
       </AppBar>
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        disableEscapeKeyDown
+        PaperProps={{
+          component: 'form',
+          onSubmit: (event) => {
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
+            const formJson = Object.fromEntries(formData.entries());
+            const email = formJson.email;
+            console.log(email);
+            handleClose();
+          },
+        }}
+      >
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We will send updates occasionally.
+          </DialogContentText>
+        </DialogContent>
+
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   );
 }
