7ca280e5704b6b0b4c976d5560fa9ab3be51f626
Toan Le <thanhtoanhcm1602@gmail.com>
2024-06-04 21:02:04 +0700

    ReactJS: 15-09 Validation: Email

--- src/features/Auth/components/RegisterForm/index.jsx
+++ src/features/Auth/components/RegisterForm/index.jsx
@@ -21,6 +21,7 @@ function RegisterForm(props) {
       .test('should has at least two words', 'Please enter at least two words.', (value) => {
         return value.split(' ').length >= 2;
       }),
+    email: yup.string().required('Please enter your email.').email('Please enter a valid email.'),
   });
 
   const form = useForm({
