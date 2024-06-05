ac8e3e98a08f0aa9ef0acc4f128600a9b66d4946
Toan Le <thanhtoanhcm1602@gmail.com>
2024-06-05 14:57:21 +0700

    ReactJS: 15-20 Parse error khi đăng ký thất bại

--- src/features/Auth/components/Register/index.jsx
+++ src/features/Auth/components/Register/index.jsx
@@ -35,6 +35,7 @@ const Register = (props) => {
       enqueueSnackbar('Register successfully!', { variant: 'success' });
     } catch (error) {
       console.log('Failed to register: ', error);
+      enqueueSnackbar(error.message, { variant: 'error' });
     }
   };
 
