47d42d3596ade792ab6a92f4b64be69b34905535
Toan Le <thanhtoanhcm1602@gmail.com>
2024-06-05 14:17:23 +0700

    ReactJS: 15-18 Hiển thị thông báo với notistack

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './app/store';
+import { SnackbarProvider } from 'notistack';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,7 +14,9 @@ root.render(
     // <React.StrictMode>
     <Provider store={store}>
         <BrowserRouter>
-            <App />
+            <SnackbarProvider anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
+                <App />
+            </SnackbarProvider>
         </BrowserRouter>
     </Provider>
     // </React.StrictMode>
