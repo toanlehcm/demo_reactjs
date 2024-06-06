a9f82ac8f200c69b2f81884f70c6b0c3046cbaf9
Toan Le <thanhtoanhcm1602@gmail.com>
2024-06-06 16:10:31 +0700

    ReactJS: 16-08 Xử lý thumbnail cho sản phẩm

--- src/features/Product/components/Product.jsx
+++ src/features/Product/components/Product.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Box, Skeleton, Typography } from '@mui/material';
+import { STATIC_HOST, THUMBNAIL_PLACEHOLDER } from 'constants/index';
 
 Product.propTypes = {
   product: PropTypes.object,
 };
 
 function Product({ product }) {
+  const thumbnailUrl = product.thumbnail ? `${STATIC_HOST}${product.thumbnail?.url}` : THUMBNAIL_PLACEHOLDER;
+
   return (
     <Box padding={1}>
-      <Skeleton variant='rectangular' width='100%' height={118} />
+      <Box padding={1}>
+        <img src={thumbnailUrl} alt={product.name} width='100%' />
+      </Box>
+
       <Typography variant='body2'>{product.name}</Typography>
       <Typography variant='body2'>
         {product.salePrice} - {product.promotionPercent}
