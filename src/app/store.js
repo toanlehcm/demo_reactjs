dec38b47cd8a266e530b4b6cbe3bab1479520c47
Toan Le <thanhtoanhcm1602@gmail.com>
2024-06-07 18:28:53 +0700

    ReactJS: 18-03 Tạo actions cho cart items

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../features/Counter/counterSlice'
 import userReducer from '../features/Auth/userSlice'
+import cartReducer from '../features/Cart/cartSlice';
 
 // rootReducer include all reducers are available in source.
 const rootReducer = {
   count: counterReducer,
   user: userReducer,
+  cart: cartReducer,
 }
 
 // Create store.
